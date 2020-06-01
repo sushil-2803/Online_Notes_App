ccb3255692e359cbd2cc85e712fa1849b68358e6
unknown <sushilanildubey@gmail.com>
2020-06-01 23:32:22 +0530

    UserNAme Validation Working

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$host="localhost";
+$user="root";
+$pwd="";
+$dbname="Notes";
+$conn=mysqli_connect($host,$user,$pwd,$dbname);
+?>
---
+++ validateusername.php
@@ -0,0 +1,14 @@
+<?php
+include 'db.php';
+$username=$_POST['username'];
+$query="SELECT * FROM users WHERE Username ='$username'";
+$res=mysqli_query($conn,$query);
+if(@mysqli_num_rows($res)>0)
+{
+    echo "User Name Already Exsits";
+}
+else
+{
+    echo "User Name Valid";
+}
+?>
